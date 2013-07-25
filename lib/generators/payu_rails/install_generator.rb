c097855ca560babfec6cb360538dc5c30c8b806f
Piotr Kowalski <piotr.kowalski@me.com>
2013-07-25 14:32:29 +0200

    Added routes to generator.

--- lib/generators/payu_rails/install_generator.rb
+++ lib/generators/payu_rails/install_generator.rb
@@ -12,6 +12,13 @@ module PayuRails
       def copy_configuration_file
         copy_file "payu.yml", "config/payu.yml"
       end
+
+      desc "Setup payu routes file."
+      def add_routes
+        route %Q{match "/payu/ok" => "payu#ok"}
+        route %Q{match '/payu/error' => 'payu#error'}
+        route %Q{match '/payu/report' => 'payu#report'}
+      end
     end
   end
 end
--- test/generator/install_generator_test.rb
+++ test/generator/install_generator_test.rb
@@ -4,11 +4,34 @@ require "generators/payu_rails/install_generator"
 class InstallGeneratorTest < Rails::Generators::TestCase
   tests PayuRails::Generators::InstallGenerator
   destination File.expand_path("../../tmp", __FILE__)
-  setup :prepare_destination
+
+  setup do
+    prepare_destination
+    copy_routes
+  end
 
   test "Assert all files are properly created" do
     run_generator
     assert_file "config/initializers/payu.rb"
     assert_file "config/payu.yml"
+  end
+
+  test "routes.rb contains specific routes" do
+    run_generator
+    match1 = /match "\/payu\/ok" => "payu#ok"/
+    match2 = /match '\/payu\/error' => 'payu#error'/
+    match3 = /match '\/payu\/report' => 'payu#report'/
+    [match1, match2, match3].each do |match|
+      assert_file "config/routes.rb", match
+    end
+  end
+
+  protected
+  def copy_routes
+    routes = File.expand_path("../../dummy/config/routes.rb", __FILE__)
+    destination = File.join(destination_root, "config")
+
+    FileUtils.mkdir_p(destination)
+    FileUtils.cp routes, destination
   end
 end
